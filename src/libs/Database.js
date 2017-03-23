693c6062d28377cdf07da62dc7de9a46e3daf54f
Nattawut <nattawut@thinknet.co.th>
2017-03-23 17:24:14 +0700

    add database class

---
+++ src/libs/Database.js
@@ -0,0 +1,30 @@
+import * as firebase from 'firebase'
+
+class Database {
+  constructor(database) {
+    // Set the configuration for your app
+    // TODO: Replace with your project's config object
+    const config = {
+      apiKey: database.apiKey,
+      authDomain: database.authDomain,
+      databaseURL: database.databaseURL,
+      storageBucket: database.storageBucket,
+      messagingSenderId: database.messagingSenderId,
+    }
+    firebase.initializeApp(config)
+
+    // Get a reference to the database service
+    this.db = firebase.database()
+  }
+
+  saveData(name, yesterday, today, date) {
+    this.db.ref(`daily/${date}_${name}`).set({
+      name,
+      yesterday,
+      today,
+      date,
+    })
+  }
+}
+
+export default Database
