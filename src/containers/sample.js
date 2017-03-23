62c01e706b992f851619369050e8e206720d1997
Nattawut <nattawut@thinknet.co.th>
2017-03-23 14:39:42 +0700

    add sample container

---
+++ src/containers/sample.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Sample extends Component {
+  state = {}
+
+  funcSomething = () => {
+
+  }
+
+  render() {
+    return (
+      <Sample />
+    );
+  }
+}
+
+export default JobDetail;
