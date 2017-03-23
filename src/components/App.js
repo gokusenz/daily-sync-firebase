b09e3ec1c0c0ba34426f53e12da1a552e975a4fa
Nattawut <nattawut@thinknet.co.th>
2017-03-23 19:05:29 +0700

    change default page

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ import logo from '../../public/images/logo.svg'
 const App = ({ children }) => (
   <div className="App">
     <div className="App-header">
-      <h2>Welcome to ReactJS</h2>
       <img src={logo} className="App-logo" alt="logo" />
     </div>
     <p>{ children }</p>
