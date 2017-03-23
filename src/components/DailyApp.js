9bba746956bb2a442c65007a98ca1e617d9b886c
Nattawut <nattawut@thinknet.co.th>
2017-03-23 13:19:45 +0700

    add route (coe)

--- src/components/DailyApp.js
+++ src/components/DailyApp.js
@@ -1,14 +1,16 @@
 import React, { PropTypes } from 'react'
+import DailyForm from './DaiyForm'
 import './App.scss'
 import logo from '../../public/images/logo.svg'
 
 const App = ({ children }) => (
   <div className="App">
     <div className="App-header">
-      <h2>Welcome to ReactJS</h2>
+      <h2>Welcome to Daily Sync (COE)</h2>
       <img src={logo} className="App-logo" alt="logo" />
     </div>
     <p>{ children }</p>
+    <DailyForm />
   </div>
 )
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import App from '../components/App'
+import DailyApp from '../components/DailyApp'
 
 const Routes = () => (
   <div>
     <Route exact path="/" component={App} />
+    <Route exact path="/coe" component={DailyApp} />
   </div>
 )
 
