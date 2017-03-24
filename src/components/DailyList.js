615842ab5575f78b4e1a48784509b46ca66d4e7e
Nattawut <nattawut@thinknet.co.th>
2017-03-24 13:55:25 +0700

    fix style

--- src/components/DailyList.js
+++ src/components/DailyList.js
@@ -8,13 +8,15 @@ const DailyList = ({ team, dailyList }) => (
     <div className="App-header">
       <h2>Daily List of ({team})</h2>
       <img src={logo} className="App-logo" alt="logo" />
-      <ul className="list-group">
-        {
-          dailyList.map(doc => (
-            <ListItem key={doc.id} item={doc} />
-          ))
-        }
-      </ul>
+      <div className="col-xs-12 App-List">
+        <ul className="list-group">
+          {
+            dailyList.map(doc => (
+              <ListItem key={doc.id} item={doc} />
+            ))
+          }
+        </ul>
+      </div>
     </div>
   </div>
 )
