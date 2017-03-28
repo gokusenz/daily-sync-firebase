0283104ee5f0eec3753bf16fe072b705b805e5d8
Nattawut <nattawut@thinknet.co.th>
2017-03-28 16:37:12 +0700

    edit DailyReport

--- src/containers/DailyReport.js
+++ src/containers/DailyReport.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import DailyReportComponent from '../components/DailyReport'
 import Database from '../libs/Database'
 import DateLib from '../libs/Date'
+import LineApi from '../libs/LineApi'
 
 export class DailyReport extends Component {
   constructor() {
@@ -10,6 +11,7 @@ export class DailyReport extends Component {
     this.state = {
       dailyList: [],
       chooseDate: '',
+      handleReport: this.handleReport.bind(this),
     }
   }
 
@@ -29,6 +31,35 @@ export class DailyReport extends Component {
     })
   }
 
+  handleReport(team) {
+    let reportList
+    const chooseDate = DateLib.getCurDate()
+    let msg = `\nReport : https://daily-sync-app.firebaseapp.com/report/${team}\n\n${chooseDate} #${team}\n\n`
+    this.database.getList(chooseDate, this.props.team)
+    .then((result) => {
+      const arr = []
+      const r = result.val()
+      for (const i in r) {
+        arr.push({ id: i, ...r[i] })
+      }
+      reportList = arr
+
+      reportList.map(doc => (
+        msg = msg.concat(doc.name)
+              .concat('\nวันนี้ทำอะไร\n')
+              .concat(doc.yesterday)
+              .concat('\nเมื่อวานทำอะไร\n\n')
+              .concat(doc.today)
+              .concat('\n\n')
+      ))
+      console.log(msg)
+      LineApi.lineNotify(msg)
+      .then((lineResult) => {
+        console.log(lineResult)
+      })
+    })
+  }
+
   render() {
     const { team } = this.props
     return (
@@ -36,6 +67,7 @@ export class DailyReport extends Component {
         date={this.state.chooseDate}
         team={team}
         dailyList={this.state.dailyList}
+        handleReport={this.state.handleReport}
       />
     )
   }
