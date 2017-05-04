a38dece4139bd6ce4c6493421e28cae52c02f908
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-05-04 14:12:59 +0700

    move route fodler to route.js

--- src/common/reducers/index.js
+++ src/common/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
 import FieldReducer from './FieldReducer'
 
-const rootReducer = combineReducers({
+export default combineReducers({
   field: FieldReducer,
+  routing: routerReducer,
 })
-
-export default rootReducer
---
+++ src/common/routes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Router, Route, IndexRoute, IndexRedirect, Redirect } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import DailyApp from './components/DailyApp'
+import DailyReportsContainer from './containers/DailyReport'
+
+const routes = (store, history) => (
+  <Router
+    history={syncHistoryWithStore(history, store)}
+    onUpdate={() => window.scrollTo(0, 0)}
+  >
+    <Route path="/">
+      <Route path="coe" component={() => <DailyApp team="COE" />} />
+      <Route path="report/coe" component={() => <DailyReportsContainer team="COE" />} />
+      <Redirect from="*" to="/coe" />
+    </Route>
+  </Router>
+)
+
+export default routes
