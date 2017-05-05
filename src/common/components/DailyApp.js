8b3674e1246c11bbaa5ad8ac1e713614358df3b1
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-05-05 15:18:45 +0700

    change call image from import to absolute path

--- src/common/components/DailyApp.js
+++ src/common/components/DailyApp.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import DailyForm from '../containers/DailyForm'
 import './App.scss'
-import logo from '../../../public/images/logo.svg'
 
 const DailyApp = ({ team }) => (
   <div className="App">
     <div className="App-header">
       <h2>Daily Sync v1.2.0 ({team})</h2>
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src="/public/images/logo.svg" className="App-logo" alt="logo" />
     </div>
     <DailyForm team={team} />
   </div>
